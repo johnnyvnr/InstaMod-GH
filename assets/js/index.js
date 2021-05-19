3c556f5edc4eb795dd9036e625c990b5ac19c363
João Victor Nascimento Rueda <69706617+johnnyvnr@users.noreply.github.com>
2021-05-19 20:58:47 -0300

    Initial commit

---
+++ assets/js/index.js
@@ -0,0 +1,8 @@
+let btn = document.getElementById('btn');
+function Toggle(){
+    if(btn.classList.contains("far")){
+        btn.classList.remove("far");
+        btn.classList.add("faslike")
+        btn.classList.add("fas");
+    };
+};
